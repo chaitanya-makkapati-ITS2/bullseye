42ed518d5ac9ed08d562c6815029643e41c41c76
chaitanya-makkapati-ITS2 <chaitanya.makkapati@its.ny.gov>
2024-12-06 14:36:58 -0500

    Initial Commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Chaitanya Makkapati on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bullseyeTests/bullseyeTests.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeTests.swift
+//  bullseyeTests
+//
+//  Created by Chaitanya Makkapati on 12/6/24.
+//
+
+import Testing
+@testable import bullseye
+
+struct bullseyeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
